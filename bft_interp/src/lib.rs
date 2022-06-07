8f35a42e05cdb2b592235085d5f66e97070f2e15
Chris Shepherd <chris.shepherd@codethink.co.uk>
2022-06-07 10:11:03 +0100

    Part 3 (bring together), fix Cargo.toml
    For some reason, my Cargo.toml was unhappy. To fix, I think I just
    changed the order of {workspace, dependencies}. One of those where
    finding out what I did wrong takes far longer than fixing!

--- bft_interp/src/lib.rs
+++ bft_interp/src/lib.rs
@@ -1,2 +1,25 @@
+use bft_types::BFProgram;
+
+pub struct VM<T> {
+    num_cells: usize,
+    tape: Vec<T>,
+    can_grow: bool,
+}
+
+impl<T> VM<T> {
+    pub fn new(size: usize, can_grow: bool) -> Self {
+        let mut num_cells = 30000;
+        if size != 0 {
+            num_cells = size;
+        };
+        let tape = Vec::with_capacity(num_cells);
+        Self {
+            num_cells,
+            tape,
+            can_grow,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use bft_interp::VM;
+use bft_types::BFProgram;
+use std::{env, error::Error};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Nice syntax taken from Nathan's solution
+    // ok_or: converts Option<T> into Result<T,E>
+    let file_name = env::args().nth(1).ok_or("No file name provided")?;
+
+    // How could the error propagation for opening the file be put into
+    // bft_types/src/lib.rs?
+    let prog = BFProgram::from_file(file_name)?;
+    let vm: VM<usize> = VM::new(0, false);
+
+    Ok(())
 }
